7e877e869081a0cac5b88324b4797b3e216ec324
ALHWARY <nnytttdh@gmail.com>
2026-09-10 14:03:12 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "nexachat",
+  "version": "1.0.0",
+  "private": true,
+  "description": "Username-based realtime chat with private and group messaging",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "better-sqlite3": "^11.8.1",
+    "express": "^4.21.2",
+    "socket.io": "^4.8.1"
+  }
+}
